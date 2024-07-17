afab5a7de3b15633f03f74f04b8eb1e39e7f18ba
Антон Ветров <55150813+Anton554@users.noreply.github.com>
2024-07-18 00:51:51 +0300

    работает на телефоне

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS
-from PIL import Image, ImageDraw
+import numpy as np
+import cv2
 import base64
-import io
-import os
+import threading
+from queue import Queue
+from ultralytics import YOLO
+import ssl
 
 app = Flask(__name__, static_folder='static')
 socketio = SocketIO(app, cors_allowed_origins="*")
 CORS(app)
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+frame_counter = 0  # Глобальный счетчик кадров
+model = YOLO('yolov8n.pt')  # Загружаем модель YOLO здесь
+last_results = None  # Последние результаты для рисования рамок
 
+frame_queue = Queue(maxsize=10)  # Очередь для кадров
 
 @app.route('/')
 def index():
@@ -20,53 +26,102 @@ def index():
 
 @socketio.on('send_frame')
 def handle_frame(data):
+    global frame_counter, last_results
+    frame_counter += 1
+
+    frame = decode_frame(data)
+    if frame is not None:
+        frame_queue.put(frame)  # Добавляем кадр в очередь
+
+    if frame_counter % 7 == 0:  # Обрабатываем и отправляем сообщение только для каждого пятнадцатого кадра
+        threading.Thread(target=process_frame, args=(frame,)).start()
+
+
+def process_frame(frame):
+    global last_results
     try:
-        processed_image = process_frame(data)
-        emit('receive_frame', {'image': processed_image})
+        last_results = model(frame)[0]  # Обработка кадра моделью YOLO
+        message = summarize_results(last_results)
+        socketio.emit('receive_message', {'message': message})  # Отправляем сообщение о найденных объектах
     except Exception as e:
         print(f"Error processing frame: {e}")
+        last_results = None  # Обнуляем результаты в случае ошибки
+
+    while not frame_queue.empty():
+        frame = frame_queue.get()
+        if last_results:
+            frame = draw_boxes(frame, last_results)
+        encoded_frame = encode_frame(frame)
+        socketio.emit('receive_frame', {'image': encoded_frame})  # Отправляем изображение с рамками
 
 
-def process_frame(data):
+def decode_frame(data):
     try:
-        image = decode_image(data)
-        image = draw_rectangle(image)
-        return encode_image(image)
+        if not data:
+            print("Received empty frame data")
+            return None
+        
+        frame_data = base64.b64decode(data)
+        buffer_array = np.frombuffer(frame_data, np.uint8)
+        
+        if buffer_array.size == 0:
+            print("Buffer array is empty")
+            return None
+        
+        frame = cv2.imdecode(buffer_array, cv2.IMREAD_COLOR)
+        
+        if frame is None:
+            print("Failed to decode frame")
+        
+        return frame
     except Exception as e:
-        print(f"Error in process_frame: {e}")
-        raise
+        print(f"Error decoding frame: {e}")
+        return None
 
 
-def decode_image(data):
+def summarize_results(results):
+    counts = {}
+    class_names = results.names  # Доступ к именам классов
+    for result in results.boxes:
+        class_name = class_names[int(result.cls)]
+        if class_name in counts:
+            counts[class_name] += 1
+        else:
+            counts[class_name] = 1
+    return ', '.join([f"{value} {key}(s)" for key, value in counts.items()])
+
+
+def draw_boxes(frame, results):
     try:
-        image_data = base64.b64decode(data)
-        return Image.open(io.BytesIO(image_data))
+        for result in results.boxes:
+            coords = result.xyxy[0].tolist()  # Преобразование тензора в список
+            x1, y1, x2, y2 = map(int, coords)
+            class_name = results.names[int(result.cls)]
+            confidence = float(result.conf)  # Преобразование тензора в скаляр
+
+            # Рисуем прямоугольник
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            
+            # Подписываем объект
+            label = f"{class_name} ({confidence:.2f})"
+            cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        
+        return frame
     except Exception as e:
-        print(f"Error decoding image: {e}")
-        raise
+        print(f"Error drawing boxes: {e}")
+        return frame
 
 
-def draw_rectangle(image):
+def encode_frame(frame):
     try:
-        draw = ImageDraw.Draw(image)
-        width, height = image.size
-        rectangle_specs = (width // 4, height // 4, width * 3 // 4, height * 3 // 4)
-        draw.rectangle(rectangle_specs, outline="red", width=10)
-        return image
+        _, buffer = cv2.imencode('.jpg', frame)
+        return base64.b64encode(buffer).decode('utf-8')
     except Exception as e:
-        print(f"Error drawing rectangle: {e}")
-        raise
-
-
-def encode_image(image):
-    try:
-        buffer = io.BytesIO()
-        image.save(buffer, format="JPEG", quality=95)
-        return base64.b64encode(buffer.getvalue()).decode('utf-8')
-    except Exception as e:
-        print(f"Error encoding image: {e}")
-        raise
+        print(f"Error encoding frame: {e}")
+        return None
 
 
 if __name__ == '__main__':
-    socketio.run(app, allow_unsafe_werkzeug=True, debug=True, host='0.0.0.0', port=5000)
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    context.load_cert_chain(certfile='cert.pem', keyfile='key.pem')
+    socketio.run(app, allow_unsafe_werkzeug=True, debug=True, host='0.0.0.0', port=5001, ssl_context=context)
--- static/js/app.js
+++ static/js/app.js
@@ -1,79 +1,140 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Установка соединения с сервером через Socket.IO
-    const socket = io.connect('http://localhost:5001');
+    const socket = io.connect('https://192.168.3.37:5001');
 
-    // Обработка успешного соединения
     socket.on('connect', function() {
         console.log("Socket connected successfully");
     });
 
-    // Обработка ошибок соединения
     socket.on('connect_error', (error) => {
         console.error('Connection Error:', error);
     });
 
-    // Нахождение элементов в DOM
     const video = document.querySelector('video');
     const canvas = document.querySelector('canvas');
     const context = canvas.getContext('2d');
+    const detectionsDiv = document.getElementById('detections'); // Элемент для вывода детекций
     const startButton = document.getElementById('startButton');
+    const cameraSelect = document.getElementById('cameraSelect');
     let localStream = null;
+    let lastValidDetection = ""; // Хранение последнего действительного сообщения о детекциях
+    let videoDevices = [];
+    let animationFrameId;
 
-    // Обработчик нажатия на кнопку 'Start Camera'
     startButton.onclick = async function() {
         if (!localStream) {
             try {
-                // Запрос доступа к медиа (камере)
-                localStream = await navigator.mediaDevices.getUserMedia({ video: true });
-                video.srcObject = localStream;
-                video.play();
+                await getVideoDevices();
+                await startCamera();
                 sendFramePeriodically();
             } catch (error) {
                 alert('Failed to get video stream. Please ensure the camera is connected and allowed.');
             }
         } else {
-            // Остановка потока видео
             stopVideoStream();
         }
     };
 
-    // Функция для остановки видео потока
+    cameraSelect.onchange = async function() {
+        await switchCamera();
+    };
+
+    async function getVideoDevices() {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        videoDevices = devices.filter(device => device.kind === 'videoinput');
+        cameraSelect.innerHTML = '';
+
+        videoDevices.forEach((device, index) => {
+            const option = document.createElement('option');
+            option.value = device.deviceId;
+            option.text = device.label || `Camera ${index + 1}`;
+            cameraSelect.appendChild(option);
+        });
+
+        if (videoDevices.length > 1) {
+            cameraSelect.style.display = 'block';
+        } else {
+            cameraSelect.style.display = 'none';
+        }
+
+        // Установите значение селектора камеры на заднюю камеру, если она доступна
+        const defaultCamera = videoDevices.find(device => device.label.toLowerCase().includes('back') || device.label.toLowerCase().includes('rear'));
+        if (defaultCamera) {
+            cameraSelect.value = defaultCamera.deviceId;
+        } else {
+            cameraSelect.value = videoDevices[0].deviceId; // В качестве запасного варианта используйте первую камеру
+        }
+    }
+
+    async function startCamera() {
+        if (localStream) {
+            localStream.getTracks().forEach(track => track.stop());
+        }
+
+        if (videoDevices.length > 0) {
+            const videoConstraints = {
+                deviceId: { exact: cameraSelect.value },
+                facingMode: 'environment' // Добавьте это, чтобы указать заднюю камеру
+            };
+            localStream = await navigator.mediaDevices.getUserMedia({ video: videoConstraints });
+            video.srcObject = localStream;
+            video.play();
+        } else {
+            alert('No camera found');
+        }
+    }
+
+    async function switchCamera() {
+        stopVideoStream();
+        await startCamera();
+    }
+
     function stopVideoStream() {
         if (localStream) {
             localStream.getTracks().forEach(track => track.stop());
             localStream = null;
             video.srcObject = null;
         }
+        if (animationFrameId) {
+            cancelAnimationFrame(animationFrameId);
+        }
     }
 
-    // Периодическая отправка кадров на сервер
     function sendFramePeriodically() {
         if (video.paused || video.ended || !localStream) return;
-        canvas.width = video.videoWidth * 0.7;
-        canvas.height = video.videoHeight * 0.7;
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
         context.drawImage(video, 0, 0, canvas.width, canvas.height);
-        const data = canvas.toDataURL('image/jpeg', 1);
-        socket.emit('send_frame', data.split(',')[1]);
-        requestAnimationFrame(sendFramePeriodically);
+        const data = canvas.toDataURL('image/jpeg').split(',')[1]; // Уменьшение качества JPEG
+        socket.emit('send_frame', data);
+        animationFrameId = requestAnimationFrame(sendFramePeriodically);
     }
 
-    // Обработка получения кадра от сервера
     socket.on('receive_frame', function(data) {
-        if (!data.image) {
-            return;
+        if (data.image) {
+            const img = new Image();
+            img.onload = function() {
+                const processedCanvas = document.getElementById('processedCanvas');
+                const processedContext = processedCanvas.getContext('2d');
+                processedCanvas.width = img.width;
+                processedCanvas.height = img.height;
+                processedContext.clearRect(0, 0, processedCanvas.width, processedCanvas.height);
+                processedContext.drawImage(img, 0, 0, processedCanvas.width, processedCanvas.height);
+            };
+            img.src = 'data:image/jpeg;base64,' + data.image;
         }
-        const img = new Image();
-        img.onload = function() {
-            const processedCanvas = document.getElementById('processedCanvas');
-            const processedContext = processedCanvas.getContext('2d');
-            processedCanvas.width = img.width;
-            processedCanvas.height = img.height;
-            processedContext.clearRect(0, 0, processedCanvas.width, processedCanvas.height);
-            processedContext.drawImage(img, 0, 0, processedCanvas.width, processedCanvas.height);
-        };
-        img.onerror = function() {
-            console.error("Error loading the processed image.");
-        };
-        img.src = 'data:image/jpeg;base64,' + data.image;
+    });
+
+    socket.on('receive_message', function(data) {
+        if (data.message && data.message !== 'Frame skipped') {
+            lastValidDetection = data.message; // Обновление последнего валидного сообщения
+            detectionsDiv.style.display = 'block';
+            detectionsDiv.textContent = lastValidDetection;
+        } else if (data.message === 'Frame skipped') {
+            // Показываем последнее валидное сообщение, если текущее - "Frame skipped"
+            if (lastValidDetection) {
+                detectionsDiv.style.display = 'block';
+                detectionsDiv.textContent = lastValidDetection;
+            }
+        }
     });
 });
